57736ee386a7f357773de9158af8956975be74ed
Nikita <nikitadevj178@gmail.com>
2023-10-15 11:31:29 +0300

    Исправление файла Broken.java на github [100111]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,26 +2,26 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    String name;
+    private String name;
 
-    public String surname;
+    private int sizeOfEmpty = 10;
+
+    private String surname;
 
     public static final String NEWVALUE = "";
 
-    void broken() { }
+    public Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e) {
+    public void method(int a, int b, int c, int d, int e) {
 
     }
-
-    private int sizeOfEmpty = 10;
 
 }
